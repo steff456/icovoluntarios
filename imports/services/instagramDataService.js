2940f55eca0f86a0b953b91c6730b4a7dc31df02
Maria Camila Hernandez <mc.hernandez1@uniandes.edu.co>
2018-10-28 23:34:56 -0500

    Tarbajo con Instagram

---
+++ imports/services/instagramDataService.js
@@ -0,0 +1,19 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+
+const name = 'instagramDataServiceModule';
+
+export default angular.module(name, [
+        angularMeteor,
+    ])
+    .factory('instagramDataService', ['$http', function($http) {
+        let token = sessionStorage.getItem('ig-token');
+        return {
+            getRecentUserMedia: function(callback){
+                const endpoint = `https://api.instagram.com/v1/users/self/media/recent/?access_token=${token}&callback=JSON_CALLBACK`;
+                $http.jsonp(endpoint).success(function(response){
+                    callback(response.data);
+                });
+            }
+        }
+    }]);
