e38f85b58c9b6095b33b5f626f3b297452444f64
Maria Camila Hernandez <mc.hernandez1@uniandes.edu.co>
2018-10-30 00:10:40 -0500

    Calendario y preguntas

--- client/main.js
+++ client/main.js
@@ -1,4 +1,13 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { render } from 'react-dom';
+import '../imports/startup/accounts-config.js'; 
+import App from '../imports/ui/App.js';
+import '../imports/ui/scheduler/scheduler.js';
 import './main.html';
+
+Meteor.startup(() => {
+  render(<App />, document.getElementById('render-target'));
+});
--- server/main.js
+++ server/main.js
@@ -1,4 +1,6 @@
 import { Meteor } from 'meteor/meteor';
+import '../imports/api/tasks.js';
+import '../imports/api/events.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
